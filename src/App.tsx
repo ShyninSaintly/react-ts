01122d36b85fa62209d997f733638772cdaed617
Gleb <mamaka.s.pistoletom@gmail.com>
2025-02-10 16:49:00 +0300

    project-first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, {CardVariant} from './components/Card.tsx';
+import UserList from "./components/UserList";
+import {IUser} from "./types/types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+const users:IUser[]=[
+    {id:1,name:'Gleb',email:'gleb@gleb.com',adress:{city:'Moscow',street:'Yudkowsky',zipcode:'123'}},
+    {id:2,name:'Kemal',email:'kemal@kemal.com',adress:{city:'Krim',street:'Sevastopol',zipcode:'321'}}
+];
+    return (
+        <div>
+            <Card  variant={CardVariant.outlined} width='200px' height='200px'>
+                <button>кнопка</button>
+            </Card>
+            <UserList users={}/>
+        </div>
+    );
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
